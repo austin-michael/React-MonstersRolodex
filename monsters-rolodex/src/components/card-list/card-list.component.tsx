49ef8d758c74fabf9403399a100946595515b67a
austin-michael <austin.michaelwill@gmail.com>
2023-07-19 09:49:26 -0600

    Converted to TypeScript

--- monsters-rolodex/src/components/card-list/card-list.component.tsx
+++ monsters-rolodex/src/components/card-list/card-list.component.tsx
@@ -1,8 +1,13 @@
-import Card from "../card/card.component.jsx";
+import { Monster } from "../../App";
+import Card from "../card/card.component";
 
 import "./card-list.styles.css";
 
-const CardList = ({ monsters }) => {
+type CardListProps = {
+  monsters: Monster[];
+};
+
+const CardList = ({ monsters }: CardListProps): JSX.Element => {
   return (
     <div className="card-list">
       {monsters.map((monster) => {
